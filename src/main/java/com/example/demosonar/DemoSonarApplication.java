c45cf23faae24da7711249db2fea5deb12a2a2c4
berracode <juanguillermoalarcon@gmail.com>
2023-10-10 16:50:02 -0500

    first commit

---
+++ src/main/java/com/example/demosonar/DemoSonarApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSonarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSonarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosonar/DemoSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSonarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
